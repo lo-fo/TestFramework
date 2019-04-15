eb1c4c14d5c896f0ba19e89feb80d0e5172a0c7d
Louis Fournier <louisfournier@Louiss-MacBook.local>
2019-04-15 18:06:43 +0200

    bonjour

---
+++ TestFramework/Bonjour.swift
@@ -0,0 +1,14 @@
+//
+//  Bonjour.swift
+//  TestFramework
+//
+//  Created by Louis Fournier on 15/04/2019.
+//  Copyright © 2019 Unique Entertainment Experience. All rights reserved.
+//
+
+import Foundation
+
+
+public func bonjour() {
+  print("bonjour")
+}
