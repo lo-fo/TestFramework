2e5373359cef38f6252175c5d3d36893f89a660a
Louis Fournier <louisfournier@Louiss-MacBook.local>
2019-04-15 17:48:13 +0200

    init

---
+++ TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Louis Fournier on 15/04/2019.
+//  Copyright © 2019 Unique Entertainment Experience. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
